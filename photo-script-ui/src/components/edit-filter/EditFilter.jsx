df7801a706d4d98284db8c36defb6d58fd806a04
Darren Shin <hshin@catalyte.io>
2021-03-27 04:42:55 -0400

    completed image editing

--- photo-script-ui/src/components/edit-filter/EditFilter.jsx
+++ photo-script-ui/src/components/edit-filter/EditFilter.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import styles from './EditFilter.module.scss'
 
-const EditFilter = () => {
+const EditFilter = ({name, handleChange, active, handleClick, min, max, value}) => {
   return(
     <div>
-      Hello.
+      <div className={styles.nameContainer}>
+        <p className={styles.name} onClick={handleClick}>{name}</p>
+      </div>
+      {active &&
+      <div className={styles.sliderContainer}>
+        <input
+          type="range"
+          className={styles.slider}
+          min={min}
+          max={max}
+          value={value}
+          onChange={handleChange}
+        />
+      </div>}
     </div>
   )
 }
